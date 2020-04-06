902a8386aa06bcb594cb6e7a7ae43369ce524947
Shawn-911 <ashawnmichael@gmail.com>
2020-04-05 23:19:06 -0400

    fixed my images card and added a delete button into the view modal works kinda wack tho and started the edit modal

--- client/main.js
+++ client/main.js
@@ -24,6 +24,7 @@ Template.myJumbo.events({
   	'click .js-exitAdd'(event, instance){
     console.log("closing modal");
     $("#bName").val("");
+    $("bAuth").val("");
     $("#path").val("");
     $("#desc").val("");
     $(".placeHolder").attr("src","imgPlaceHolder.png");
@@ -66,10 +67,55 @@ Template.mainBody.events({
   $('#viewContent').html(booksdb.findOne({_id:myID}).desc);
 },
 
+'click .js-delete'(event,instance){
+  var myID=this._id;
+  $("#"+this._id).fadeOut('slow',function(){
+    booksdb.remove({_id:myID});
+    console.log(myID);
+    
+  });
+  console.log("deleting...");
+    $("#viewBook").modal("hide");
+},
+
   'input #path'(event, instance){
      console.log($('#path').val());
     $(".brath").attr("scr",$("#path").val());
+  },
+
+
+'click. js-edit'(event,instance){
+  $("#editModal").modal("show");
+  var myId = this._id;
+    // console.log("let's edit "+myId);
+    var editName = booksdb.findOne({_id: myId}).bName;
+    var editAuth = booksdb.findOne({_id: myId}).bAuth
+    var editpath = booksdb.findOne({_id: myId}).path;
+    var editdesc = booksdb.findOne({_id: myId}).desc;
+    $("#editId").val(myId);
+    $("#editName").val(editName);
+    $("#editAuth").val(editAuth);
+    $("#editpath").val(editpath);
+    $("#editdesc").val(editdesc);
+    $(".editHolder").attr("src", editpath);
+},
+  'click.js-editsave'(event,instance){
+     var editName = $("#editName").val();
+     var editAuth = $("#editAuth").val();
+    var editpath = $("#editpath").val();
+    var editdesc = $("#editdesc").val();
+    var updateId = $("#editId").val();
+    console.log("updating "+updateId+" Image with title: "+editName+"and its author with "+editAuth+" and its path is "+edithpath+" and its description "+editdesc);
+    booksdb.update({_id: updateId},
+      {$set:{
+        "bName": editName,
+        "bAuth": editAuth,
+        "path": editpath,
+        "desc": editdesc
+      }}
+    );
   }
+
   });
 
 
